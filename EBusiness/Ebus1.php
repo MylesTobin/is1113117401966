cbcfa32245b1516751e2424ee6bfcafa5945445e
EC2 Default User <ec2-user@ip-172-31-45-180.eu-west-1.compute.internal>
2018-03-08 15:19:43 +0000

    update

---
+++ EBusiness/Ebus1.php
@@ -0,0 +1,80 @@
+<?php
+//Start the session
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Select Products</title>
+
+<!--jQuery--->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script type="text/javascript" src="cost_calc.js"></script>
+    
+</head>
+<center><body>
+<h4> Select a product </h4>
+<br/>
+    <form method ="POST" action ="Ebus2.php">
+<label for "aws"> 
+<input type="radio" id="aws" name="product" checked onClick="disabledbtnProceed()"/>
+AWS @300
+</label>
+<br/>
+<br/>
+<label for "salesforce"> 
+<input type = "radio" id = "salesforce" name = "product" onClick = "disabledbtnProceed()"/>
+SalesForce @ 100
+</label>
+<br/>
+<br/>
+<label for "cloud9"> 
+<input type = "radio" id = "cloud9" name = "product" onClick = "disabledbtnProceed()"/>
+Cloud 9 @ 200
+</label>
+<br/>
+<br/>
+<label for "gmail"> 
+<input type = "radio" id = "gmail" name = "product" onClick = "disabledbtnProceed()"/>
+Gmail @ 400
+</label>
+<br/>
+<br/>
+<label for="subtotal">
+Sub Total
+<input type = "text" id = "subtotal" value = "0.00" readonly/>
+</label>
+<br/>
+<br/>
+<label for="discount">
+Discount
+<input type = "text" id = "discount" value = "0.00" readonly/>
+</label>
+<br/>
+<br/>
+<label for="vat">
+V.A.T.
+<input type = "text" id = "vat" value = "0.00" readonly/>
+</label>
+<br/>
+<br/>
+<label for="total">
+Total
+<input type = "text" id = "total" value = "0.00" readonly/>
+</label>
+        
+        <br/>
+        <br/>
+        
+<button type "submit" id = "btnProceed" disabled>Add to Shopping Cart </button>
+</form>
+<br/>
+<br/>
+<button onClick="calcSub()">Calculate Cost </button>
+<br/>
+<br/>
+<a role = "button" href = "Ebus1.php"> Clear Choice </a>
+</body></center>
+
+</html>
---
+++ EBusiness/cost_calc.js
@@ -0,0 +1,65 @@
+/* global $ */
+
+function calcSub(){
+    
+    var argSubTotal;
+    
+    if(document.getElementById('salesforce').checked){
+      argSubTotal = 100;
+    }
+    
+    else if(document.getElementById('gmail').checked){
+        argSubTotal = 400;
+    }
+    
+    else if(document.getElementById('cloud9').checked){
+        argSubTotal = 200;
+    }
+    
+    else{
+        argSubTotal = 300;
+    }
+
+   
+    calcDisVatTotal(argSubTotal);
+}
+   
+   function calcDisVatTotal(parmSubTotal){
+       
+       var SubTotal;
+       var discountAmt;
+       var vatAmt;
+       var totalAmt;
+     
+       SubTotal = parmSubTotal;
+       
+       discountAmt = SubTotal * 0.05;
+       
+       vatAmt = (SubTotal - discountAmt) * 0.1;
+       
+       totalAmt = (SubTotal + vatAmt) - discountAmt;
+       
+       display(SubTotal, discountAmt, vatAmt, totalAmt);
+   }
+   
+   
+  
+    function display(parm1, parm2, parm3, parm4){
+        
+        document.getElementById("subtotal").value =  parm1;
+        document.getElementById("discount").value = parm2;
+        document.getElementById("VAT").value =  parm3;
+        document.getElementById("total").value = parm4;
+        
+        
+        enablebtnProceed();
+    }
+    
+    
+    function enablebtnProceed(){
+        $('#btnProceed').prop('disabled', false);
+    }
+    
+    function disablebtnProceed(){
+        $('#btnProceed').prop('disabled', true);
+    }
