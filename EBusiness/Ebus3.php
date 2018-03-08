505bea6bc2941ba1f154efe2bba29c33ed63bc1c
EC2 Default User <ec2-user@ip-172-31-45-180.eu-west-1.compute.internal>
2018-03-08 17:11:35 +0000

    update

--- EBusiness/Ebus3.php
+++ EBusiness/Ebus3.php
@@ -15,13 +15,6 @@
          </div>   
     </body>
     
-      <footer>
-        <a href="https://ucc.ie">
-            <img position ="static" src="https://www.cubsucc.com/images/layout/uccLogo.png"/>
-        </a>
-        <a id="cubs" href="https://www.cubsucc.com/">
-            <img position = "absolute" src="https://www.cubsucc.com/images/layout/logo.png"/>
-        </a>
-    </footer>
+      
     
 </html>
--- EBusiness/ebus2_validator.js
+++ EBusiness/ebus2_validator.js
@@ -7,10 +7,10 @@ function validateDetails(){
     pin = document.getElementById("user_pin").value;
     
     if (pin==""){
-        alert("Please enter your PIN");
+        alert("Please enter a valid PIN");
         }
     else if (String(pin).length<4){
-        alert("Please make sure your PIN is accurate");
+        alert("Please make sure your PIN is valid");
         }
     else{
         enablebtnPurchase();
@@ -23,4 +23,19 @@ function enablebtnPurchase(){
 
 function disablebtnPurchase(){
      $('#btnPurchase').prop('disabled',true);
+}
+
+
+function validateDetails(){
+    
+    var name;
+    
+    name = document.getElementById("user_pin").value;
+    
+    if (name==""){
+        alert("Please enter a valid Name")
+        else{
+            enablebtnPurchase()
+        }
+    }
 }
